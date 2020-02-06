9a636e109fcf9b9f1820aa5612185ce6fe91c165
Yongkun Li <indusdesign@163.com>
2020-02-05 22:37:41 -0500

    basic function completed

---
+++ Week1-CodeLab0-Refresh/Assets/Scripts/DropsProperty.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropsProperty : MonoBehaviour
+{
+    public GameObject mainThread;
+    public int id;
+    public float r;
+    private Rigidbody2D rb2D;
+
+    // Input
+    private bool rightForce;
+    private bool leftForce;
+    private bool upForce;
+    private bool downForce;
+
+    void Start()
+    {
+        rb2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // detect input
+        rightForce = Input.GetKey(KeyCode.D);
+        leftForce = Input.GetKey(KeyCode.A);
+        upForce = Input.GetKey(KeyCode.W);
+        downForce = Input.GetKey(KeyCode.S);
+
+        if (rightForce) rb2D.AddForce(new Vector3(r, 0.0f, 0.0f));
+        if (leftForce) rb2D.AddForce(new Vector3(-r, 0.0f, 0.0f));
+        if (upForce) rb2D.AddForce(new Vector3(0.0f, r, 0.0f));
+        if (downForce) rb2D.AddForce(new Vector3(0.0f, -r, 0.0f));
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.GetComponent<DropsProperty>()){
+            if(this.r >= other.gameObject.GetComponent<DropsProperty>().r){
+                this.r = this.r+other.gameObject.GetComponent<DropsProperty>().r/2;
+                this.gameObject.transform.localScale = new Vector2(this.r, this.r);
+            }else{
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Week1-CodeLab0-Refresh/Assets/Scripts/StartGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    public GameObject Drop;
+    public int totalDropsAmount = 10;
+    public static int dropsCount;
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        dropsCount = 0;
+        for (; dropsCount < totalDropsAmount; dropsCount++) {
+            Drop.GetComponent<DropsProperty>().id = dropsCount;
+            Drop.GetComponent<DropsProperty>().r = Random.Range(0.5f, 1.0f);
+            Drop.transform.localScale = new Vector2(Drop.GetComponent<DropsProperty>().r, Drop.GetComponent<DropsProperty>().r);
+            Instantiate(Drop, new Vector2(Random.Range(-3.0f, 3.0f), Random.Range(-3.0f, 3.0f)), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
